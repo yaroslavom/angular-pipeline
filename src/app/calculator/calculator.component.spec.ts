130cf6c04e099ed399248ed3045a5c762980a243
Yaroslav <72073598+yaroslavom@users.noreply.github.com>
2026-07-22 12:18:18 +0200

    add: broke the test case to get an error in the pipeline testing phase

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -30,7 +30,7 @@ describe('CalculatorComponent', () => {
     component.calculatorForm.get('operator').setValue('+');
     component.onSubmit();
 
-    expect(component.result).toBe(10);
+    expect(component.result).toBe(11);
 
   });
 
